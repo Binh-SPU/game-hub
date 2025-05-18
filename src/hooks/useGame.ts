7494f1e125323ffc71001b71a631f2fae2d32218
Binh-SPU <phamb5@spu.edu>
2025-05-18 01:30:58 -0700

    Refactoring Move Entities

--- src/hooks/useGame.ts
+++ src/hooks/useGame.ts
@@ -1,13 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import ApiClient from "@/services/api-client";
-import { Game } from "./useGames";
+import { Game } from "../entities/Game";
 
 const apiClient = new ApiClient<Game>("/games");
 
 const useGame = (slug: string) =>
-  useQuery<Game, Error>(
-    ["game", slug],
-    () => apiClient.get(slug) 
-  );
+  useQuery<Game, Error>(["game", slug], () => apiClient.get(slug));
 
 export default useGame;
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -2,14 +2,9 @@ import { useQuery } from "@tanstack/react-query";
 import ApiClient, { FetchReponse } from "@/services/api-client";
 import platforms from "@/data/platforms";
 import ms from "ms";
+import { Platform } from "../entities/Platform";
 
 const apiClient = new ApiClient<Platform>("/platforms/lists/parents");
-
-export interface Platform {
-  id: number;
-  name: string;
-  slug: string;
-}
 
 const usePlatforms = () =>
   useQuery<FetchReponse<Platform>, Error>(["platforms"], apiClient.getAll, {
