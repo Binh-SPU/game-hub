1522d700a078b231c715781ec6223513b3fbae62
Binh-SPU <phamb5@spu.edu>
2025-05-18 00:54:00 -0700

    Setting Up Routing

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,27 +1,22 @@
-import { Box, Grid, GridItem, HStack } from "@chakra-ui/react";
-import GameGrid from "./components/GameGrid";
-import GameHeading from "./components/GameHeading";
-import GenreList from "./components/GenreList";
-import NavBar from "./components/NavBar";
-import PlatformSelector from "./components/PlatformSelector";
-import SortSelector from "./components/SortSelector";
-import "./index.css";
+import GameGrid from '@/components/GameGrid';
+import GameHeading from '@/components/GameHeading';
+import GenreList from '@/components/GenreList';
+import PlatformSelector from '@/components/PlatformSelector';
+import SortSelector from '@/components/SortSelector';
+import { Grid, Box, GridItem, HStack } from '@chakra-ui/react';
 
-function App() {
+const HomePage = () => {
   return (
     <Grid
       templateAreas={{
-        base: `"nav" "main"`,
-        lg: `"nav nav" "aside main"`,
+        base: `"main"`,
+        lg: `"aside main"`,
       }}
       templateColumns={{
         base: "1fr",
         lg: "200px 1fr",
       }}
     >
-      <GridItem area="nav">
-        <NavBar />
-      </GridItem>
       <Box display={{ base: "none", lg: "block" }}>
         <GridItem area="aside" paddingX={5}>
           <GenreList />
@@ -41,4 +36,4 @@ function App() {
   );
 }
 
-export default App;
+export default HomePage
