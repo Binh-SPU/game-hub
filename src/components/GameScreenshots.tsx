d23e23b936aca90606337f1a87b42ca3f333157c
Binh-SPU <phamb5@spu.edu>
2025-05-18 19:59:25 -0700

    Build Game Screenshot

---
+++ src/components/GameScreenshots.tsx
@@ -0,0 +1,24 @@
+import useScreenshots from "@/hooks/useScreenshots";
+import { Image } from "@chakra-ui/react";
+import { SimpleGrid } from "@chakra-ui/react";
+
+interface GameScreenshotsProps {
+  gameId: number;
+}
+
+const GameScreenshots = ({ gameId }: GameScreenshotsProps) => {
+  const { data, isLoading, error } = useScreenshots(gameId);
+
+  if (isLoading) return;
+  if (error) throw error;
+
+  return (
+    <SimpleGrid columns={{ base: 1, md: 2 }} spacing={2}>
+      {data?.results.map((screenshot) => (
+        <Image key={screenshot.id} src={screenshot.image} />
+      ))}
+    </SimpleGrid>
+  );
+};
+
+export default GameScreenshots;
